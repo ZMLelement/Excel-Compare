0eb7c0c75da7b2c612f029daf7049a1d2050d1a3
pvdwijdeven <pvdwijdeven@gmail.com>
2017-05-04 13:54:09 +0200

    Add files via upload

---
+++ mindmap/Excel compare.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1493890265043" ID="ID_915825581" MODIFIED="1493890272288" TEXT="Excel compare"/>
+</map>
